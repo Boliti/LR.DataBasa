802ba37a6c6dad1d3d74ea4cfc702db09a7d4383
Nikita Yalov <wolfox.nikita@gmail.com>
2023-10-16 10:27:57 +0400

    2.5

---
+++ LR.2/Task-2-5.sql
@@ -0,0 +1,3 @@
+USE cd;
+SELECT * FROM facilities  WHERE membercost != 0 and membercost/monthlymaintenance <= 1/50;
+--Выбрать объекты, которые платны для членов клуба, но стоимость использование не превышает 1/50 от стоимость месячного обслуживания 
