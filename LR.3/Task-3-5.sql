6266c6548db69d1b3e8f31d694825bc939a85d7c
Nikita Yalov <wolfox.nikita@gmail.com>
2023-10-24 12:09:21 +0400

    3.5

--- LR.3/Task-3-5.sql
+++ LR.3/Task-3-5.sql
@@ -2,4 +2,4 @@ USE cd;
 /* Выберите ФИО (== имя + фамилия) всех, кто покупал корты 1 и 2. */
 SELECT DISTINCT surname, firstname FROM members 
 JOIN bookings ON members.memid = bookings.memid 
-WHERE  bookings.facid IN (0, 1);
+WHERE  bookings.facid IN (0, 1) AND members.memid !=0;
