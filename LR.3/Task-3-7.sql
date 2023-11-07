9c2949be979cc6aff6363de3459e7b3b9a91c189
Nikita Yalov <wolfox.nikita@gmail.com>
2023-11-07 11:32:35 +0400

    3.7

--- LR.3/Task-3-7.sql
+++ LR.3/Task-3-7.sql
@@ -2,10 +2,7 @@ USE cd;
 /* Выберите список всех членов, включая человека, который их рекомендовал (если таковой имеется), 
 без использования каких-либо объединений. Исключите в списке дубликаты,
  упорядочите лист по ФИО (==   имя + фамилия). */
-WITH new AS (
-    SELECT CONCAT(m1.surname, " ", m1.firstname) AS fiomember 
-    FROM members m1, members m2
-    WHERE m1.recommendedby = m2.memid OR m1.recommendedby IS NULL
-)
-SELECT DISTINCT fiomember FROM new
-WHERE fiomember NOT LIKE "%GUEST%" ORDER BY fiomember;
+SELECT CONCAT(m1.surname, ' ', m1.firstname) AS fiomember, (SELECT CONCAT(m2.surname, ' ', m2.firstname) 
+FROM members m2 WHERE m2.memid = m1.recommendedby) AS fiorecommendedby FROM members m1
+WHERE m1.memid != 0
+ORDER BY fiomember;
