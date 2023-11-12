6f44dde179f1a17e96436a9816ac88680e5c4c36
Nikita Yalov <wolfox.nikita@gmail.com>
2023-11-12 15:01:37 +0400

    4.9

--- LR.4/Task-4-2.sql
+++ LR.4/Task-4-2.sql
@@ -1,9 +1,10 @@
 /*Добавить (одновременно)  два сквош-корта (2 и 3 == ‘Squash Court 2’, ‘Squash Court 3’) */
 USE cd;
+/* изменяем membercost для того чтобы можно писать не только целые числа */
 ALTER TABLE facilities
 MODIFY membercost
 DECIMAL(10,1);
-
+/* изменяем guestcost для того чтобы можно писать не только целые числа */
 ALTER TABLE facilities
 MODIFY guestcost
 DECIMAL(10,1);
@@ -12,3 +13,11 @@ INSERT INTO facilities (facid, facility, membercost, guestcost, initialoutlay, m
 VALUES (10, 'Squash Court 2', 3.5, 17.5, 5000, 80),
        (11, 'Squash Court 3', 3.5, 17.5, 5000, 80);
 SELECT * FROM facilities;
+
+
+
+
+
+/* Удаление строк с facid = (10,11) (для проверки работы скрипта) */
+DELETE FROM facilities
+WHERE facid IN(10,11);
