955d9a66a6a2b3b469d9d011a3feae214a244510
Nikita Yalov <wolfox.nikita@gmail.com>
2023-11-21 13:08:31 +0400

    5.8-5.10

---
+++ LR.5/Task-5-9.sql
@@ -0,0 +1,9 @@
+USE cd;
+/*Создайте список объектов с общим доходом менее 1000. Составьте список объектов вместе с их общим доходом. Выходная таблица должна состоять из названия объекта и доходов, 
+отсортированных по доходам.*/
+SELECT facility, SUM(CASE 
+    WHEN b.memid = 0 THEN f.guestcost * b.slots 
+    ELSE f.membercost * b.slots 
+END) AS SUM from facilities f
+LEFT JOIN bookings b ON b.facid = f.facid
+GROUP BY f.facility HAVING SUM > 1000 ORDER BY SUM DESC;
