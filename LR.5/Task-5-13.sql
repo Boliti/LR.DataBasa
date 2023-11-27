418a47fcd6a3bbbda7cdc8809f73b131f22db24a
Nikita Yalov <wolfox.nikita@gmail.com>
2023-11-27 10:01:01 +0400

    5-15

--- LR.5/Task-5-13.sql
+++ LR.5/Task-5-13.sql
@@ -1,5 +1,5 @@
 /*Создайте монотонно увеличивающийся нумерованный список участников (включая гостей), упорядоченный по дате присоединения. 
 Помните, что идентификаторы участников не обязательно будут последовательными. Используйте функцию нумерации row_number.*/
-SELECT ROW_NUMBER() OVER (ORDER BY joindate) AS 'Номер', memid AS 'Идентификатор участника', 
-CONCAT(firstname,' ', surname) AS 'ФИО', joindate AS 'Дата присоединения' 
+SELECT ROW_NUMBER() OVER (ORDER BY joindate) AS 'Номер', memid AS ID, 
+CONCAT(firstname,' ', surname) AS fiomember, joindate 
 FROM members ORDER BY joindate;
