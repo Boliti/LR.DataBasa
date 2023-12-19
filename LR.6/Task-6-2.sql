14b028b1ffdb5528fa63fab5b746d74325a9a840
Nikita Yalov <wolfox.nikita@gmail.com>
2023-12-19 11:49:26 +0400

    LR.6.2

--- LR.6/Task-6-2.sql
+++ LR.6/Task-6-2.sql
@@ -1,5 +1,7 @@
+use cd;
 /*Выберите количество аренд объектов клуба в каждый месяц.*/
-SELECT YEAR(b.starttime) AS 'Год', MONTH(b.starttime) AS 'Месяц', f.facility AS 'Название объекта', COUNT(*) AS 'Количество аренд в каждый месяц'
-FROM bookings b
-INNER JOIN facilities f ON b.facid = f.facid
-GROUP BY YEAR(starttime), MONTH(starttime), facility;
+SELECT YEAR(b.starttime), MONTH(b.starttime), 
+       f.facility, COUNT(*)
+  FROM bookings b, facilities f 
+  WHERE b.facid = f.facid
+  GROUP BY YEAR(starttime), MONTH(starttime), facility;
