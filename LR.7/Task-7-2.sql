902a93591d0c903200e0db044bd29ae31fb7ec4c
Nikita Yalov <wolfox.nikita@gmail.com>
2023-12-25 10:45:14 +0400

    task

---
+++ LR.7/Task-7-2.sql
@@ -0,0 +1,65 @@
+/*Создайте таблицу payments со структурой (payid INT PK, FK on booking.bookid; payment  DECIMAL.)*/
+USE cd;
+CREATE TABLE payments (
+  payid INT PRIMARY KEY AUTO_INCREMENT,
+  bookid INT,
+  payment DECIMAL DEFAULT 0,
+  FOREIGN KEY (bookid) REFERENCES bookings(bookid)
+);
+/*Добавьть в таблицу bookings поле payed, смысл которого оплачена или не оплачена аренда.*/
+ALTER TABLE bookings ADD payed TINYINT DEFAULT 0;
+/*событие на запрет удаления оплаченных бронирований*/
+DELIMITER $$
+DROP TRIGGER IF EXISTS prevent_payment_deletion $$
+CREATE TRIGGER prevent_payment_deletion 
+BEFORE DELETE ON bookings 
+FOR EACH ROW 
+BEGIN
+  IF (OLD.payed = 1) THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Can not delete a paid booking!';
+  END IF;
+END$$
+DELIMITER ;
+/*2) После отметки оплаты, заносят в таблицу payments запись с соответствующим значением PK 
+и суммой оплаты, для вычисления которой используется функция созданная в Task-7-1.*/
+/*3) При отмене оплаты - удаляет соответствующую запись в таблице payments.*/
+DELIMITER $$
+/*В данном триггере происходит удаление и замена записи*/
+DROP TRIGGER IF EXISTS payment_record $$
+CREATE TRIGGER payment_record 
+AFTER UPDATE ON bookings
+FOR EACH ROW
+BEGIN
+    CASE
+        WHEN NEW.payed = OLD.payed THEN BEGIN END;
+        WHEN NEW.payed = 1 THEN
+            INSERT INTO payments(bookid, payment)
+            VALUES(NEW.bookid, CalculateRentalCost(NEW.memid, NEW.facid, NEW.slots));
+        WHEN NEW.payed = 0 THEN
+            DELETE FROM payments p WHERE p.bookid = NEW.bookid;
+    END CASE;
+END $$
+/*Если бронь изменила свой статус оплаты, то удаляет\добавляет оплату (в ином случае делает ничего)*/
+DROP TRIGGER IF EXISTS payed_in_book $$
+CREATE TRIGGER payed_in_book 
+AFTER INSERT ON bookings 
+FOR EACH ROW
+BEGIN
+    IF NEW.payed = 1 THEN
+        INSERT INTO payments(bookid, payment)
+        VALUES(NEW.bookid, CalculateRentalCost(NEW.memid, NEW.facid, NEW.slots));
+    END IF;
+END $$
+DELIMITER ;
+/*изменение статуса записей*/
+UPDATE bookings 
+SET payed = 1
+WHERE DATE(starttime) < '2012-08-01' AND DATE(starttime) >= '2012-07-01';
+/*Посчитайте (написав соответствующий скрипт) оплату на июль 2012 года двумя способами: 1) используя данные таблицы payments*/
+SELECT SUM(payment) as July1
+FROM payments;
+/*2) используя только функцию из Task-7-1 и данные таблицы bookings.*/
+SELECT SUM(CalculateRentalCost(memid, facid, slots)) as July2
+FROM bookings 
+WHERE DATE(starttime) < '2012-08-01' AND DATE(starttime) >= '2012-07-01';
+/*Вывод: при подсчете July1 и July2 выдали одинаковые значения*/
--- LR.8/Task1.sql
+++ LR.8/Task1.sql
@@ -1,10 +1,10 @@
 /* Самый кассовый фильм месяца */
 SELECT f.titleFIL AS 'Название фильма', 
-       SUM(a.number_of_tickets * p.cost) AS 'Выручка' 
+       MONTH(p.startDate) AS 'Месяц', SUM(a.number_of_tickets * p.cost) AS 'Выручка'
 FROM Films f 
 INNER JOIN Poster p ON f.idFIL = p.film 
 INNER JOIN Audience a ON p.idPOS = a.poster_number 
-GROUP BY f.titleFIL 
+GROUP BY f.titleFIL, p.startDate
 ORDER BY SUM(a.number_of_tickets * p.cost) DESC 
 LIMIT 1;
 
@@ -13,19 +13,29 @@ SELECT C.titleCIN, C.address, C.number_of_places - SUM(A.number_of_tickets) AS f
 FROM Cinemas C
 JOIN Poster P ON C.idCIN = P.cinema
 LEFT JOIN Audience A ON P.idPOS = A.poster_number
-WHERE P.startDate <= '2022-01-19' AND P.endDate >= '2022-01-01'
 GROUP BY C.idCIN, A.visiting_day
 HAVING freeseats > 0
 ORDER BY freeseats DESC;
 
 /* Фильм с указанием максимального количества зрителей. */
-SELECT F.titleFIL, MAX(A.total_tickets) AS max_daily_audience
+SELECT F.titleFIL, MAX(A.total_tickets) AS max_daily_audience, DAY(a.visiting_day)
 FROM Films F
 JOIN Poster P ON F.idFIL = P.film
 JOIN (
-    SELECT poster_number, SUM(number_of_tickets) AS total_tickets
+    SELECT poster_number, visiting_day, SUM(number_of_tickets) AS total_tickets
     FROM Audience
-    GROUP BY poster_number
+    GROUP BY poster_number, visiting_day
 ) A ON P.idPOS = A.poster_number
-GROUP BY F.idFIL
+GROUP BY F.titleFIL, a.visiting_day
 ORDER BY max_daily_audience DESC;
+
+
+SELECT F.titleFIL, A.visiting_day, A.max_daily_audience
+FROM Films F
+JOIN (
+    SELECT P.film, A.visiting_day, MAX(A.total_tickets) AS max_daily_audience
+    FROM Poster P
+    JOIN Audience A ON P.idPOS = A.poster_number
+    GROUP BY P.film, A.visiting_day
+) A ON F.idFIL = A.film
+ORDER BY F.titleFIL, A.visiting_day;
